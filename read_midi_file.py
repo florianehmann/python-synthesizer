c8eddb9efa2c274363e065e873157e78d475a2c7
Florian Ehmann <kontakt@florianehmann.de>
2023-05-10 15:15:50 +0200

    Read Example MIDI File

---
+++ read_midi_file.py
@@ -0,0 +1,11 @@
+import mido
+
+if __name__ == '__main__':
+    mid = mido.MidiFile('music/simple_test.mid')
+    print(mid)
+    for msg in mid.tracks[0]:
+        match msg:
+            case mido.MetaMessage(type='time_signature'):
+                print(f'time_signatue={msg.numerator}/{msg.denominator}')
+            case mido.MetaMessage(type='set_tempo'):
+                print(f'tempo={msg.tempo}')
